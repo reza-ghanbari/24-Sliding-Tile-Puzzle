28fe9b50a42e255966cbe585b5dcc9da5c98f675
reza-ghanbari <rezaqanbari1925@gmail.com>
2023-04-17 20:53:13 -0600

    add the set of problem cases and change the main to read problems from the file

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <chrono>
 #include <algorithm>
+#include <sstream>
 #include "inc/NeighborCache.h"
 #include "inc/Heuristic.h"
 #include "inc/Solver.h"
@@ -24,9 +25,27 @@ Heuristic* getHeuristic(std::vector<Short>& tiles, NeighborCache* neighborCache,
     }
 }
 
-State* getRoot() {
-    std::vector<Short> state = {10, 3, 24, 12, 0, 7, 8, 11, 14, 21, 22, 23, 2, 1, 9, 17, 18, 6, 20, 4, 13, 15, 5, 19, 16};
-//    std::vector<Short> state = {2, 17, 4, 13, 7, 12, 10, 3, 0, 16, 21, 24, 8, 5, 18, 20, 15, 19, 14, 9, 22, 11, 6, 1, 23};
+std::pair<std::vector<Short>, std::vector<std::vector<Short>>> readRootsFromFile(std::string filename) {
+    std::ifstream infile(filename);
+    std::string line;
+    std::vector<Short> root;
+    std::vector<Short> problemNumber;
+    std::vector<std::vector<Short>> roots;
+    while (std::getline(infile, line)) {
+        std::istringstream iss(line);
+        int number;
+        iss >> number;
+        problemNumber.push_back(number);
+        while (iss >> number) {
+            root.push_back(number);
+        }
+        roots.push_back(root);
+        root.clear();
+    }
+    return std::make_pair(problemNumber, roots);
+}
+
+State* getRoot(std::vector<Short> state) {
     std::vector<Short> dual;
     for (Short i = 0; i < 25; i++) {
         dual.push_back(std::find(state.begin(), state.end(), i) - state.begin());
@@ -46,20 +65,26 @@ int main() {
     if (WRITE_PDB) {
         irregularHeuristic->saveToFile("pdb-irregular.txt");
     }
-    State* root = getRoot();
-    auto* solver = new Solver(regularHeuristic, irregularHeuristic, neighborCache);
-    if (SOLVE) {
-        auto start = std::chrono::steady_clock::now();
-        std::vector<Short> path = solver->solve(root);
-        auto end = std::chrono::steady_clock::now();
-        root->printState();
-        std::cout << "Solution length: " << path.size() - 1 << std::endl;
-        std::cout << "Time difference for solving = "
-                  << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << "[s]"
-                  << std::endl;
-        std::cout << "Path: ";
-        for (auto &step: path) {
-            std::cout << unsigned(step) << " ";
+    auto *solver = new Solver(regularHeuristic, irregularHeuristic, neighborCache);
+    auto problems = readRootsFromFile("../res/problems.txt");
+    int i = 0;
+    for (const auto& rootState: problems.second) {
+        State *root = getRoot(rootState);
+        if (SOLVE) {
+            std::cout << "Solving problem " << unsigned (problems.first[i++]) << std::endl;
+            auto start = std::chrono::steady_clock::now();
+            std::vector<Short> path = solver->solve(root);
+            auto end = std::chrono::steady_clock::now();
+            root->printState();
+            std::cout << "Solution length: " << path.size() - 1 << std::endl;
+            std::cout << "Time difference for solving = "
+                      << std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count() / 1000.0 << "[s]"
+                      << std::endl;
+            std::cout << "Path: ";
+            for (auto &step: path) {
+                std::cout << unsigned(step) << " ";
+            }
+            std::cout << "\n\n\n" << std::endl;
         }
     }
     return 0;
